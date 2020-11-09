cec4cf518981a1eb3a46ee41f65cb11086e74c53
GitHub Action <action@github.com>
2020-11-09 14:55:05 +0000

    Bump version to 0.1.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "silence-operator"
 	source      = "https://github.com/giantswarm/silence-operator"
-	version     = "0.1.1"
+	version     = "0.1.2-dev"
 )
 
 func Description() string {
