6d572df03f1387a79a1425b6f1ab6d3d37eb9280
Theo Brigitte <theo.brigitte@gmail.com>
2023-02-23 17:33:02 +0100

    add comments

--- service/controller/key/key.go
+++ service/controller/key/key.go
@@ -39,14 +39,20 @@ func SilenceComment(silence v1alpha1.Silence) string {
 	return fmt.Sprintf("%s-%s", CreatedBy, silence.Name)
 }
 
+// SilenceValidUntil gets the expiry date for a given silence.
+// The expiry date is retrived from the valid-until label.
+// The expected format is 2006-01-02
+// It returns a invalidValidUntilDateError in case the date format is invalid.
 func SilenceValidUntil(silence v1alpha1.Silence) (time.Time, error) {
 	labels := silence.GetLabels()
 
+	// Check if the label exist otherwise return a date 1000 years in the future.
 	value, ok := labels[ValidUntilLabelName]
 	if !ok {
 		return eternity, nil
 	}
 
+	// Parse the date found in the label.
 	validUntilTime, err := time.Parse(time.DateOnly, value)
 	if err != nil {
 		return time.Time{}, microerror.Maskf(invalidValidUntilDateError, "valid-until date %q does not match expected format %q: %v", value, time.DateOnly, err)
