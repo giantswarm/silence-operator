e78b1481d6a5d7885a8b185b9bcfae7abd217737
taylorbot <dev@giantswarm.io>
2023-10-12 08:09:07 +0000

    Bump version to 0.11.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "silence-operator"
 	source      = "https://github.com/giantswarm/silence-operator"
-	version     = "0.11.1"
+	version     = "0.11.2-dev"
 )
 
 func Description() string {
